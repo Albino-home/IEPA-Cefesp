7b8175b6c3162f594cf513dadc2ee91518135f90
Albino-home <albinovilingatomas3@gmail.com>
2025-08-28 23:35:56 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const root = document.getElementById('root');
+root.innerHTML = `
+  <div style="text-align:center; font-family: Arial, sans-serif;">
+    <h1 style="color:blue;">IEPA-Cefesp</h1>
+    <p style="color:red;">Seja bem-vindo ao aplicativo oficial da nossa igreja.</p>
+    <button onclick="alert('Obrigado por visitar a IEPA-Cefesp!')">Clique aqui</button>
+  </div>
+`;
